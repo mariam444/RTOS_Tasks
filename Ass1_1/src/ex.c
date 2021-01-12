3f8e20bb151547a0cf2cd0705ce6562e815bbdba
mariam444 <mariamsayed444@gmail.com>
2021-01-12 08:41:13 +0200

    all file done

---
+++ Ass1_1/src/ex.c
@@ -0,0 +1,21 @@
+/*
+ * Name: header
+ * Author: Mariam Sayed
+ * Description : Understanding MACRO concept
+ * Date : 13/11/2020
+ */
+
+#include<stdio.h>
+#include"header.h"
+
+int main (void)
+{
+	int x =5 , r = 2 ;
+	int z ;
+	z=GET_BIT(x,r);
+	printf("%d \n",z);
+
+	return 0;
+	}
+
+
---
+++ Ass1_1/src/header.h
@@ -0,0 +1,16 @@
+/*
+ * Name: header
+ * Author: Mariam Sayed
+ * Description : Understanding MACRO concept
+ * Date : 13/11/2020
+ */
+
+
+#ifndef header
+#define header
+
+#define GET_BIT(REG,BIT) ( ( REG & (1 << BIT ) ) >> BIT)
+
+
+
+#endif
